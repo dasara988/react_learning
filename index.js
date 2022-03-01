b09bf7fdfcca9f9640645cd86cc78e19d46fa324
dasara988 <dasaradhinnv143@gmail.com>
2022-03-01 17:03:38 +0000

    practice_1

---
+++ index.js
@@ -0,0 +1,23 @@
+const element = (
+  <div className="mainBlock">
+    <div className="textBlock">
+      <h1>Super Over league</h1>
+    </div>
+    <div className="teamBlock">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        alt="Image not load"
+        width="100"
+        height="100"
+      />
+      <img
+        src=" https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        alt="Image not load"
+        width="100"
+        height="100"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
